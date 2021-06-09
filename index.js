d8ae54e9699e6f2da8d1d2fb7acccab61d33654e
Josiane-Rocha <josie.rocha.93@gmail.com>
2021-06-08 23:28:44 -0300

    Quarto Commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ connection.connect()
 
 
 // 1 - Serviço de retorno de lista de todos os produtos existentes
-app.get('/projeto/v1/produtos', (req, res) => {
+app.get('/produtos', (req, res) => {
 
     // Busca produtos   
     connection.query('SELECT id, produto, preco, disponivel FROM sistema_ecommerce.produtos', function(err, rows, fields) {
@@ -32,7 +32,7 @@ app.get('/projeto/v1/produtos', (req, res) => {
 
 
 // 2 - Serviço de retorno de produto
-app.get('/projeto/v1/departamentos/:departamentoId/produtos/:produtoId', (req, res) => {
+app.get('/produtos/:produtoId/departamentos/:departamentoId', (req, res) => {
 
   // Busca de retorno de detalhes de 1 único produto
   connection.query('SELECT id, codigo, produto, preco, disponivel FROM sistema_ecommerce.produtos WHERE id_departamento = ' + req.params.departamentoId + ' AND id = ' + req.params.produtoId, function(err, rows, fields) {
@@ -45,32 +45,36 @@ app.get('/projeto/v1/departamentos/:departamentoId/produtos/:produtoId', (req, r
 
 
 // 3 - Serviço de inclusao de produto na base de dados 
-app.post('projeto/v1/produtos', (req, res) => {
+app.post('/produtos/inclui', (req, res) => {
 
 
     // Busca de retorno de produto na base de dados
-    connection.query('SELECT id, codigo, produto, preco, disponivel FROM sistema_ecommerce.produtos WHERE id_departamento = ' + req.params.departamentoId), function(err, rows, fields) {
-      if (err) throw err
+    connection.query ('const {id, produto, preco, disponivel}') = req.body;
+    if (err) throw err
 
-      res.send(rows)
-    }
-})  
+    res.send(rows[0])
+    res.json({id, produto, preco, disponivel});
+
+  });
+
 
 
 // 4 - Serviço de retorno de atualização na base de dados
-app.put('/projeto/v1/departamentos', (req, res) => {
+app.put('/produtos/atualiza', (req, res) => {
 
   // Busca atualização na base de dados
-  connection.query('SELECT id, nome FROM sistema_ecommerce.departamentos', function(err, rows, fields) {
-      if (err) throw err
-
-      res.send(rows)
+  const {id} = req.params;
+    const product = data.find(pro => pro.id == id);
+    if (!product) return res.status(204).json();
+    const {preco, codProduto} = req.body;
+    product.preco = preco;
+    res.json(product);
     })
 })
 
 
 // 5 - Serviço de retorno de lista de todos os departamentos
-app.get('/projeto/v1/departamentos', (req, res) => {
+app.get('/departamento', (req, res) => {
 
     // Busca departamentos    
     connection.query('SELECT id, nome FROM sistema_ecommerce.departamentos', function(err, rows, fields) {
@@ -82,7 +86,7 @@ app.get('/projeto/v1/departamentos', (req, res) => {
 
 
 // 6 - Serviço de retorno de departamentos e lista de produtos
-app.get('/projeto/v1/departamentos/:departamentoId', (req, res) => {
+app.get('/departamentos/:departamentoId/produtos', (req, res) => {
 
     // Busca de departamentos e lista de produtos
     connection.query('SELECT id, codigo, produto, preco, disponivel FROM sistema_ecommerce.produtos WHERE id_departamento = ' + req.params.departamentoId, function(err, rows, fields) {
